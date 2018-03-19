6855d93a3cb2ad712e40ac13dddb1014d21d1492
Bruno Soares <bruno@bsoares.com.br>
2018-03-18 21:19:25 -0300

    Spec improvements

--- spec/whats/api_spec.rb
+++ spec/whats/api_spec.rb
@@ -3,34 +3,36 @@
 require "spec_helper"
 
 RSpec.describe Whats::Api do
-  let(:base_path) { "http://whats.test" }
+  subject(:api) { described_class.new(base_path) }
 
-  subject { described_class.new(base_path) }
+  let(:base_path) { WebmockHelper::BASE_PATH }
 
-  let(:client) { double request: nil }
-
-  let(:action) { double call: nil }
+  let(:client) { instance_double("Whats::Client") }
 
   before do
     allow(Whats::Client).to receive(:new).with(base_path).and_return client
   end
 
   describe "#check_contacts" do
+    let(:action) { instance_double("Whats::Actions::CheckContacts", call: "action result") }
+
     let(:numbers) { ["+5511942424242"] }
 
-    it "calls the specific action" do
+    before do
       allow(Whats::Actions::CheckContacts)
         .to receive(:new)
         .with(client, numbers)
         .and_return action
+    end
 
-      expect(action).to receive :call
-
-      subject.check_contacts(numbers)
+    it "calls the specific action" do
+      expect(api.check_contacts(numbers)).to eq "action result"
     end
   end
 
   describe "#check_contact" do
+    let(:action) { instance_double("Whats::Actions::CheckContacts", call: response) }
+
     let(:number) { "+5511942424242" }
 
     let(:response) do
@@ -53,14 +55,14 @@ RSpec.describe Whats::Api do
     end
 
     before do
-      allow(subject)
-        .to receive(:check_contacts)
-        .with([number])
-        .and_return response
+      allow(Whats::Actions::CheckContacts)
+        .to receive(:new)
+        .with(client, [number])
+        .and_return action
     end
 
     it "formats the response for a single number" do
-      result = subject.check_contact(number)
+      result = api.check_contact(number)
 
       expect(result).to eq(
         "input_number" => "+5511942424242",
@@ -71,23 +73,27 @@ RSpec.describe Whats::Api do
   end
 
   describe "#send_message" do
+    let(:action) { instance_double("Whats::Actions::SendMessage", call: "action result") }
+
     let(:username) { "5511942424242" }
 
     let(:body) { "Message" }
 
-    it "calls the specific action" do
+    before do
       allow(Whats::Actions::SendMessage)
         .to receive(:new)
         .with(client, username, body)
         .and_return action
+    end
 
-      expect(action).to receive :call
-
-      subject.send_message(username, body)
+    it "calls the specific action" do
+      expect(api.send_message(username, body)).to eq "action result"
     end
   end
 
   describe "#send_hsm_message" do
+    let(:action) { instance_double("Whats::Actions::SendHsmMessage", call: "action result") }
+
     let(:username) { "5511942424242" }
 
     let(:namespace) { "namespace" }
@@ -96,15 +102,15 @@ RSpec.describe Whats::Api do
 
     let(:params) { { key: "value" } }
 
-    it "calls the specific action" do
+    before do
       allow(Whats::Actions::SendHsmMessage)
         .to receive(:new)
         .with(client, username, namespace, element_name, params)
         .and_return action
+    end
 
-      expect(action).to receive :call
-
-      subject.send_hsm_message(username, namespace, element_name, params)
+    it "calls the specific action" do
+      expect(api.send_hsm_message(username, namespace, element_name, params)).to eq "action result"
     end
   end
 end
