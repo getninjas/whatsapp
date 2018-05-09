a73d6b56528ecd5f22ce55f436af1e7446ceee4b
Caio Salgado <caio.c.salgado@gmail.com>
2018-05-09 19:31:16 -0300

    Enable focus in specs

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -12,8 +12,8 @@ WebMock.disable_net_connect!
 
 RSpec.configure do |config|
   config.example_status_persistence_file_path = ".rspec_status"
-
   config.disable_monkey_patching!
+  config.filter_run_when_matching :focus
 
   config.expect_with :rspec do |c|
     c.syntax = :expect
